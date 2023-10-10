54589b426b9d2d9bd5aee1c611716349384d7b66
Senthil Sivaramakrishnan <senthil.s@ncs.com.sg>
2023-10-10 20:54:14 +0800

    fix: video player

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div className="video-container">
+          <h1 className="title">PAK vs SL - Live ICC Cricket</h1>
+          <div>
+            Live streaming sponsored by <b>TON</b> 😊
+          </div>
+          <video id="videoPlayer" controls autoPlay muted></video>
+        </div>
+      </div>
   );
 }
 
