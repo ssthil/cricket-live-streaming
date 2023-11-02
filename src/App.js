4985b17bf710c937342ea028977cca839fb53d78
Senthil Sivaramakrishnan <senthil.s@ncs.com.sg>
2023-11-02 17:44:30 +0800

    update: urls

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         "https://indiatoday-pdelivery.akamaized.net/indiatoday/video/2023_10/tooltips/10_oct_23_it_newstrack_pse_fullclip_1010_vt_nb_1024_512-preview.mp4"
       ); */
       setUrl(
-        "https://vg-republictvlive.akamaized.net/v1/manifest/611d79b11b77e2f571934fd80ca1413453772ac7/vglive-sk-366023/c19b28b2-853d-4c86-b4a3-27ef071931e8/3.m3u8?ads.partner=Watcho&sessionId=a55ed05d-f199-4327-9107-18c7da601730"
+        "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=19663594820&id=43662&pk=be861b5b9a5a6cd40f1da003f7fe4d944404db6135722ad9abd4498207347ba2"
       );
       // setUrl(
       //   "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=196413659337&id=43662&pk=f4a7efb169cc3e35ebc5ac271a9874b1bd0f60bb129740660f59c79bd2771f55"
@@ -29,15 +29,15 @@ function App() {
   const channels = [
     {
       name: "CRICKET",
-      url: "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=196493695891&id=43662&pk=6df0c2108cefee4a8384c6614450f499530ce1b2b71834337b25db02f014d39c",
+      url: "https://103.lfjustfor.xyz:8088/stream/lizata1/playlist.m3u8?vidictid=19663594820&id=43662&pk=be861b5b9a5a6cd40f1da003f7fe4d944404db6135722ad9abd4498207347ba2",
     },
     {
       name: "REPUBLIC",
-      url: "https://vg-republictvlive.akamaized.net/v1/manifest/611d79b11b77e2f571934fd80ca1413453772ac7/vglive-sk-366023/c19b28b2-853d-4c86-b4a3-27ef071931e8/3.m3u8?ads.partner=Watcho&sessionId=a55ed05d-f199-4327-9107-18c7da601730",
+      url: "https://weblive.republicworld.com/httppush/ottr_republic_english/chunklist_3.m3u8",
     },
     {
       name: "WION",
-      url: "https://d7x8z4yuq42qn.cloudfront.net/index.m3u8?akes=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTc0MTUwOTR9.1VnC4dICZygTwiW3JhmkTMFH_4zBUuNhHQBvScZVx2s",
+      url: "",
     },
   ];
 
